08aa97d69a3838caa691b4cc0ce54d3c2cebbf7e
Abdur Rahaman Rahi <rahiurp20@gmail.com>
2024-04-28 10:01:14 +0600

    secure auth data

--- src/auth/firebase.config.js
+++ src/auth/firebase.config.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDxal5OxGZAphM3iZJ_FeBXZ0-o_O2YN9o",
-  authDomain: "easy-touring.firebaseapp.com",
-  projectId: "easy-touring",
-  storageBucket: "easy-touring.appspot.com",
-  messagingSenderId: "774870008203",
-  appId: "1:774870008203:web:6d6bbe10a4633e46172f8e",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
